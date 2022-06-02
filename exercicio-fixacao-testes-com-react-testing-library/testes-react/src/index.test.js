72d7807641017d42fd2cdde3d5ae652e9d7487ae
Fernanda Grein <fernandagrein.fgm@outlook.com>
2022-06-02 11:56:21 -0300

    finaliza primeiros exercícios de mock Test

--- exercicio-fixacao-testes-com-react-testing-library/testes-react/src/index.test.js
+++ exercicio-fixacao-testes-com-react-testing-library/testes-react/src/index.test.js
@@ -11,5 +11,37 @@ function somaDoisNumeros() {
 }
 
 describe("Aprendendo sobre Mocks", () => {
+  it("testa se a função foi chamada", () => {
+      somaDoisNumeros = jest.fn();
+      somaDoisNumeros();
+      expect(somaDoisNumeros).toHaveBeenCalled();
+  })
+
+  it("testa o resultado da função", () => {
+    somaDoisNumeros = jest.fn().mockReturnValue(8);
     
+    expect(somaDoisNumeros()).toBe(8);
+  })
+
+  it("testa quantas vezes a função foi chamada", () => {
+      somaDoisNumeros = jest.fn();
+
+      somaDoisNumeros();
+      somaDoisNumeros();
+      somaDoisNumeros();
+
+      expect(somaDoisNumeros).toHaveBeenCalledTimes(3);
+  })
+
+  it("testa se retorna true quando o número é par", () => {
+      retornaNumeroAleatorio = jest.fn().mockReturnValue(2);
+
+      expect(divisivelPorDois()).toBe(true);
+  })
+
+  it("Testa se o retorno é falso quando o número é impar", () => {
+    retornaNumeroAleatorio = jest.fn().mockReturnValue(3);
+
+    expect(divisivelPorDois()).toBe(false);
+  })
 })
