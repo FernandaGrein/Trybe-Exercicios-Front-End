9c9d4c10594e6a3fe31a31d85c6f2739a0b354cc
Fernanda Grein <fernandagrein.fgm@outlook.com>
2022-06-21 23:05:34 -0300

    inicia o exercicio criando a store e um reducer incial

---
+++ exercicio-thunk-react-redux/thunk-react-redux/src/redux/reducer/reducer.js
@@ -0,0 +1,16 @@
+
+const INITIAL_STATE = {
+    imagePath: '',
+    isFetch: false, 
+    error:'',
+}
+
+
+const dogReducer = ( state = INITIAL_STATE, action ) => {
+    switch(action.type) {
+        default: 
+        return state;
+    }
+};
+
+export default dogReducer;
---
+++ exercicio-thunk-react-redux/thunk-react-redux/src/redux/store.js
@@ -0,0 +1,9 @@
+import { applyMiddleware, legacy_createStore as createStore } from "redux";
+import { composeWithDevTools } from "redux-devtools-extension";
+import dogReducer from "./reducer/reducer";
+import thunk from "redux-thunk";
+
+
+const store = createStore(dogReducer, composeWithDevTools(applyMiddleware(thunk)));
+
+export default store
